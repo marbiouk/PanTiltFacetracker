993c1c26415a496bfb8009b53e668c87ac041ba9
Andrew Davies <andy.marinebio@googlemail.com>
2016-02-07 13:54:20 +0000

    Upated

--- numbertracker.py
+++ numbertracker.py
@@ -1,5 +1,4 @@
 #!/usr/bin/env python
-
 import cv2, sys, time, os
 from pantilt import *
 
@@ -11,16 +10,8 @@ os.system('v4l2-ctl -p 4')
 # Frame Size. Smaller is faster, but less accurate.
 # Wide and short is better, since moving your head
 # vertically is kinda hard!
-FRAME_W = 180
-FRAME_H = 100
-
-# Default Pan/Tilt for the camera in degrees.
-# Camera range is from 0 to 180
-cam_pan = 70
-cam_tilt = 70
-# Move camera to default
-pan(cam_pan)
-tilt(cam_tilt)
+FRAME_W = 640
+FRAME_H = 480
 
 # Set up the capture with our frame size
 video_capture = cv2.VideoCapture(0)
@@ -28,12 +19,21 @@ video_capture.set(cv2.cv.CV_CAP_PROP_FRAME_WIDTH,  FRAME_W)
 video_capture.set(cv2.cv.CV_CAP_PROP_FRAME_HEIGHT, FRAME_H)
 time.sleep(2)
 
-ret, frame = video_capture.read()
-gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
-gray = cv2.equalizeHist( gray )
+# Default Pan/Tilt for the camera in degrees.
+# Camera range is from 0 to 180
+cam_pan = 90
+cam_tilt = 90
+# Move camera to default
+pan(cam_pan)
+tilt(cam_tilt)
+panlist = [90, 70, 50]
 
-picName = 'pic.png'
-cv2.imwrite(picName, gray)
+#Take image and pan 20 degrees
+for x in panlist:
+    ret, frame = video_capture.read()
+    pan(x)
+    picName = "Pic_" + str(x) + ".png"
+    cv2.imwrite(picName, frame)
 
 #cv2.imshow("Image", gray)
 cv2.waitKey(0)
